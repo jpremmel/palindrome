084a06c735bf9b911c5add7c2c3ba29e5426fdf9
Student <example@example.com>
2019-09-25 08:34:15 -0700

    Refactor code to add offerNewGame method

--- palindrome.cs
+++ palindrome.cs
@@ -9,33 +9,37 @@ public class PalindromeChecker
   public static void Main()
   {
     PalindromeChecker game = new PalindromeChecker();
-    Console.WriteLine("Welcome to the palindrome checker!");
-    Console.WriteLine("Enter a phrase to check:");
+    Console.WriteLine("Enter a phrase to see if it's a palindrome:");
     string input = Console.ReadLine();
     int distanceFromCenter = 0;
     game.CheckPalindrome(game, input, distanceFromCenter);
     if (game.IsPalindrome)
     {
       Console.WriteLine("Your phrase is a palindrome!");
+      game.offerNewGame();
     }
     else
     {
       Console.WriteLine("Your phrase is not a palindrome.");
-      Console.WriteLine("Want to check another phrase? (Enter Y / N)");
-      string newGame = Console.ReadLine();
-      if (newGame[0].ToString().ToUpper() == "Y")
-      {
-        Main();
-      }
-      else
-      {
-        Console.WriteLine("Goodbye.");
-      }
+      game.offerNewGame();
     }
   }
-   public void CheckPalindrome(PalindromeChecker game, string input, int distanceFromCenter)
+  public void offerNewGame()
   {
-    bool isPalindrome = false;
+    Console.WriteLine("Want to check another phrase? (Enter Y / N)");
+    string newGame = Console.ReadLine();
+    if (newGame[0].ToString().ToUpper() == "Y")
+    {
+      Main();
+    }
+    else
+    {
+      Console.WriteLine("Goodbye.");
+    }
+  }
+   
+  public void CheckPalindrome(PalindromeChecker game, string input, int distanceFromCenter)
+  {
     int halfway = input.Length / 2;
     if (input.Length % 2 == 0) //EVEN LENGTH INPUT
     {
@@ -48,7 +52,8 @@ public class PalindromeChecker
         distanceFromCenter++;
         game.CheckPalindrome(game, input, distanceFromCenter);
       }
-    } else if (input.Length % 2 == 1) //ODD LENGTH INPUT
+    }
+    else if (input.Length % 2 == 1) //ODD LENGTH INPUT
     {
       if (input[halfway + distanceFromCenter] == input[halfway - distanceFromCenter] && distanceFromCenter < halfway)
       {
